3e6eca1e28f9e63acb523eadccea962152e0651f
Mhlokza <you@example.com>
2023-02-16 12:08:18 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts functio
+ n
+  *Return: 0 (Success)
+  */
+int main(void)
+{
+puts("\"programming is like building a multiligual puzz
+1e");
+return (0);
+}
